7d37635cdd82aed16cd469a4a50041f3c1217927
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-01 21:33:40 +0600

    added project structure

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#07332F",
+          secondary: "#F7A582",
+          accent: "#6C6B6B",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
