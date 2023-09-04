f53cdb792e18fb48c1e91c6db916bcb180a55623
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-04 22:02:40 +0600

    added a information section

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import OurServices from '../OurServices/OurServices';
+import Information from '../Information/Information';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <OurServices></OurServices>
+            <Information></Information>
         </div>
     );
 };
