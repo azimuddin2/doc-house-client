9b9cb24567e78b2f3779f52a6fc37f8dbc7a0abc
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-10 23:31:40 +0600

    added appointment calendar section

--- src/pages/Appointment/Appointment/Appointment.js
+++ src/pages/Appointment/Appointment/Appointment.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import AppointmentCalendar from '../AppointmentCalendar/AppointmentCalendar';
 
 const Appointment = () => {
+    const [date, setDate] = useState(new Date());
+
     return (
         <div>
             <AppointmentBanner></AppointmentBanner>
+            <AppointmentCalendar date={date} setDate={setDate}></AppointmentCalendar>
         </div>
     );
 };
---
+++ src/pages/Appointment/AppointmentCalendar/AppointmentCalendar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import background from '../../../assets/Images/landing-bg.png';
+import chair from '../../../assets/Images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import './AppointmentCalendar.css';
+
+const AppointmentCalendar = ({date, setDate}) => {
+    return (
+        <section
+            style={{
+                background: `url(${background})`,
+                backgroundSize: 'cover',
+                width: '100%',
+                backgroundPosition: '100%',
+            }}
+            className='lg:px-8 background-image'
+        >
+            <div className="hero py-8 lg:py-20">
+                <div className="hero-content flex-col lg:flex-row-reverse md:flex-row-reverse">
+                    <div className='flex-1 mb-8 sm:mb-0'>
+                        <img src={chair} className="lg:w-100 rounded-lg" alt='' />
+                    </div>
+                    <div className='flex-1'>
+                        <div className="card w-80 mx-auto lg:w-96 lg:mx-auto bg-base-100 shadow-xl">
+                            <div className="card-body p-1 lg:p-8 m-0">
+                                <DayPicker
+                                    mode="single"
+                                    selected={date}
+                                    onSelect={setDate}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AppointmentCalendar;
