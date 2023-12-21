a5937cc5bf80b73a3e2df1b47dbd0a50767fbd3f
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-12-21 14:17:51 +0000

    updated addDoctor form task

--- src/pages/Dashboard/AdminPages/AddDoctor/AddDoctor.js
+++ src/pages/Dashboard/AdminPages/AddDoctor/AddDoctor.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { MdOutlineErrorOutline } from 'react-icons/md';
+import { IoIosArrowDown } from "react-icons/io";
+import { BsCheck2Circle } from "react-icons/bs";
+import uploadPhoto from '../../../../assets/Icons/upload-photo.svg';
+import Title from '../../../../components/Title/Title';
 
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
@@ -14,19 +18,18 @@ const AddDoctor = () => {
         "Pediatric Dental"
     ];
 
-    const onSubmit = () => {
-
+    const onSubmit = (data) => {
+        console.log(data);
     };
 
     return (
-        <section className='my-10'>
+        <section className='my-12'>
+            <Title heading={'Welcome to Doc House'} title={'Add a new doctor'}></Title>
             <div className='px-5 py-8 lg:p-12 w-11/12 lg:w-1/2 mx-auto bg-[#F1F5F9]'>
-
                 <form onSubmit={handleSubmit(onSubmit)}>
-
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text font-semibold">Name*</span>
+                            <span className="label-text font-bold">Name*</span>
                         </label>
                         <input
                             {...register("name", {
@@ -46,7 +49,7 @@ const AddDoctor = () => {
 
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text font-semibold">Email*</span>
+                            <span className="label-text font-bold">Email*</span>
                         </label>
                         <input
                             {...register("email", {
@@ -64,9 +67,9 @@ const AddDoctor = () => {
                         </label>
                     </div>
 
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text font-semibold">Specialty*</span>
+                    <div className="form-control relative">
+                        <label className="label" >
+                            <span className="label-text font-bold">Specialty*</span>
                         </label>
                         <select
                             {...register("specialty")}
@@ -80,9 +83,36 @@ const AddDoctor = () => {
                                 >{specialty}</option>)
                             }
                         </select>
+                        <IoIosArrowDown className='text-xl mr-3 absolute right-1 top-12' />
                     </div>
 
+                    <div className="form-control w-full my-5">
+                        <label
+                            htmlFor='image'
+                            className="input w-full h-28 cursor-pointer text-center pt-6 rounded-sm focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                        >
+                            <span className="label-text text-accent font-medium">Upload Photo</span>
+                            <div className=' flex justify-center items-center'>
+                                <img src={uploadPhoto} alt="uploadPhoto" className='mt-1' />
+                            </div>
+                        </label>
+                        <input
+                            {...register("image", {
+                                required: {
+                                    value: true,
+                                    message: 'Image is required',
+                                },
+                            })}
+                            id="image"
+                            type="file"
+                            className="hidden"
+                        />
+                        <label className="label pt-1">
+                            {errors.image?.type === 'required' && <span className="label-text-alt text-red-500 text-sm flex items-center"><MdOutlineErrorOutline className='text-lg' style={{ marginRight: '2px' }}></MdOutlineErrorOutline>{errors.image.message}</span>}
+                        </label>
+                    </div>
 
+                    <button className='btn btn-primary text-white text-lg w-full'>Save <BsCheck2Circle className='text-xl' /></button>
                 </form>
             </div>
         </section>
